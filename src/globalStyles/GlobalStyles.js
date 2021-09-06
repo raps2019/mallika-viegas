f466c16b8c31a987aaffd6a850286dd2f1ef0ea6
Douglas Colaco <douglascolaco@gmail.com>
2021-09-06 14:42:04 -0400

    changed html width and height to 100% to avoid hidden address bar issue

--- src/globalStyles/GlobalStyles.js
+++ src/globalStyles/GlobalStyles.js
@@ -14,12 +14,16 @@ const GlobalStyle = createGlobalStyle`
 body {
   background: ${(props) => props.theme.background2};
 }
+
+html { 
+  height:100%;
+  width: 100%;
+}
 `;
 
 export const GlobalWindowContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-  min-height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
